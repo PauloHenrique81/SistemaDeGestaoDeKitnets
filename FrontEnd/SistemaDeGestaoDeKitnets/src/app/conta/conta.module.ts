9fa5b0a329b251d38fb096a6331397ea87552da0
Paulo Henrique Pereira Oliveira <paulohenriqu81@gmail.com>
2024-06-13 11:18:15 -0300

    first commit

---
+++ FrontEnd/SistemaDeGestaoDeKitnets/src/app/conta/conta.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+
+import { ContaRoutingModule } from './conta.route';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { ContaAppComponent } from './conta.app.component';
+import { CustomFormsModule } from 'ng2-validation';
+import { ContaService } from './services/conta.service';
+import { ContaGuard } from './services/conta.guard';
+
+
+
+@NgModule({
+  declarations: [
+    ContaAppComponent,
+    CadastroComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ContaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CustomFormsModule
+  ],
+  providers:[
+    ContaService,
+    ContaGuard
+  ]
+})
+export class ContaModule { }
---
+++ FrontEnd/SistemaDeGestaoDeKitnets/src/app/navegacao/navegacao.module.ts
@@ -0,0 +1,42 @@
+
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { MenuComponent } from "./menu/menu.component";
+
+import { NgModule } from '@angular/core';
+import { MenuLoginComponent } from "./menu-login/menu-login.component";
+import { AcessoNegadoComponent } from "./acesso-negado/acesso-negado.component";
+
+@NgModule({
+
+  declarations: [
+    MenuComponent,
+    MenuLoginComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent,
+    AcessoNegadoComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgbModule
+  ],
+  exports:[
+    MenuComponent,
+    MenuLoginComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent,
+    AcessoNegadoComponent
+  ]
+
+})
+
+export class NavegacaoModule {}
